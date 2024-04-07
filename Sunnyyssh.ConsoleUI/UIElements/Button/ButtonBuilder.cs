dbdbd200cc4172514842ae379f2c11c720ea6d3f
sunnyyssh <blit228@mail.ru>
2024-04-07 15:16:34 +0300

    Removed unnecessary chain setters.

--- Sunnyyssh.ConsoleUI/UIElements/Button/ButtonBuilder.cs
+++ Sunnyyssh.ConsoleUI/UIElements/Button/ButtonBuilder.cs
@@ -1,14 +1,10 @@
 ﻿using System.Collections.Immutable;
-using System.Diagnostics.CodeAnalysis;
+// ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace Sunnyyssh.ConsoleUI;
 
 public sealed class ButtonBuilder : IUIElementBuilder<Button>
 {
-    private ButtonPressedHandler? _safeButtonPressedHandler;
-
-    private ButtonPressedHandler? _unsafeButtonPressedHandler;
-    
     #region Properties
     
     private ImmutableList<ConsoleKey> _ignoredKeys = ImmutableList<ConsoleKey>.Empty;
@@ -58,140 +54,6 @@ public sealed class ButtonBuilder : IUIElementBuilder<Button>
     public Size Size { get; }
     
     #endregion
-
-    #region Chain setters.
-
-    public ButtonBuilder SetOverlappingPriority(OverlappingPriority priority)
-    {
-        OverlappingPriority = priority;
-        return this;
-    }
-    
-    public ButtonBuilder SetNotFocusedBackground(Color color)
-    {
-        NotFocusedBackground = color;
-        return this;
-    }
-
-    public ButtonBuilder SetNotFocusedForeground(Color color)
-    {
-        NotFocusedForeground = color;
-        return this;
-    }
-
-    public ButtonBuilder SetNotFocusedBorderColor(Color color)
-    {
-        NotFocusedBorderColor = color;
-        return this;
-    }
-
-    public ButtonBuilder SetFocusedBackground(Color color)
-    {
-        FocusedBackground = color;
-        return this;
-    }
-
-    public ButtonBuilder SetFocusedForeground(Color color)
-    {
-        FocusedForeground = color;
-        return this;
-    }
-
-    public ButtonBuilder SetFocusedBorderColor(Color color)
-    {
-        FocusedBorderColor = color;
-        return this;
-    }
-    
-    public ButtonBuilder SetPressedBackground(Color color)
-    {
-        PressedBackground = color;
-        return this;
-    }
-
-    public ButtonBuilder SetPressedForeground(Color color)
-    {
-        PressedForeground = color;
-        return this;
-    }
-
-    public ButtonBuilder SetPressedBorderColor(Color color)
-    {
-        PressedBorderColor = color;
-        return this;
-    }
-
-    public ButtonBuilder SetText(string text)
-    {
-        Text = text;
-        return this;
-    }
-
-    public ButtonBuilder SetLoseFocusAfterPress(bool loseFocus)
-    {
-        LoseFocusAfterPress = loseFocus;
-        return this;
-    }
-
-    public ButtonBuilder SetHandledKeys(ImmutableList<ConsoleKey>? handledKeys)
-    {
-        HandledKeys = handledKeys;
-        return this;
-    }
-
-    public ButtonBuilder SetIgnoredKeys(ImmutableList<ConsoleKey> ignoredKeys)
-    {
-        IgnoredKeys = ignoredKeys;
-        return this;
-    }
-
-    public ButtonBuilder SetBorderCharSet(BorderCharSet? charSet)
-    {
-        BorderCharSet = charSet;
-        return this;
-    }
-
-    public ButtonBuilder SetBorderKind(BorderKind borderKind)
-    {
-        BorderKind = borderKind;
-        return this;
-    }
-
-    public ButtonBuilder SetShowPress(bool show)
-    {
-        ShowPress = show;
-        return this;
-    }
-
-    public ButtonBuilder SetTextHorizontalAligning(HorizontalAligning aligning)
-    {
-        TextHorizontalAligning = aligning;
-        return this;
-    }
-    
-    public ButtonBuilder SetTextVerticalAligning(VerticalAligning aligning)
-    {
-        TextVerticalAligning = aligning;
-        return this;
-    }
-
-    public ButtonBuilder RegisterPressedHandler(ButtonPressedHandler handler)
-    {
-        ArgumentNullException.ThrowIfNull(handler, nameof(handler));
-
-        _safeButtonPressedHandler += handler;
-        return this;
-    }
-
-    public ButtonBuilder UnsafeRegisterPressedHandler(ButtonPressedHandler handler)
-    {
-        ArgumentNullException.ThrowIfNull(handler, nameof(handler));
-
-        _unsafeButtonPressedHandler += handler;
-        return this;
-    }
-    
-    #endregion
     
     public Button Build(UIElementBuildArgs args)
     {
@@ -217,16 +79,6 @@ public sealed class ButtonBuilder : IUIElementBuilder<Button>
             TextHorizontalAligning = TextHorizontalAligning,
             TextVerticalAligning = TextVerticalAligning,
         };
-
-        if (_safeButtonPressedHandler is not null)
-        {
-            resultInstance.RegisterPressedHandler(_safeButtonPressedHandler);
-        }
-
-        if (_unsafeButtonPressedHandler is not null)
-        {
-            resultInstance.UnsafeRegisterPressedHandler(_unsafeButtonPressedHandler);
-        }
 
         return resultInstance;
     }
