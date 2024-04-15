368a19de4fe0eb9da947a8c77e8c8a8cdc1ed660
sunnyyssh <blit228@mail.ru>
2024-04-15 12:20:17 +0300

    One more try to fix bug in focus flow.

--- Sunnyyssh.ConsoleUI/Core/FocusFlow/FocusFlowManager/FocusFlowManager.cs
+++ Sunnyyssh.ConsoleUI/Core/FocusFlow/FocusFlowManager/FocusFlowManager.cs
@@ -288,7 +288,7 @@ internal sealed class FocusFlowManager
         IsFocused = true;
         
         // Trying to give focus to waiting.
-        if (_focusableChain.FocusedItem is null)
+        if (_focusableChain.Current?.IsWaitingFocus ?? false)
         {
             _focusableChain.MoveNextWaitingFocus();
         }
